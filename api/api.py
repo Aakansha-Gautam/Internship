68b83ded3b91c477131d6469d4cdf71043a3ded4
Aakansha2663 <aakanshagautam12@gmail.com>
2022-12-26 12:26:31 +0545

    changes made

--- api/api.py
+++ api/api.py
@@ -9,40 +9,39 @@ def getRecipeByIngredients(ingredients):
             'apiKey':f'{private.key}'      
             }
     response = requests.get(url,params=params)
-    if response.status_code == 200:
-        recipes = response.json()
-        with open('C:\\Users\\aakan\\OneDrive\\Desktop\\In\\api\\recipes.json', 'a') as file:
-            for recipe in recipes:
-                for item in recipe["missedIngredients"]:
-                    data = {
-                        'ingredients': ingredients,
-                        'id': recipe['id'],
-                        'missedIngredientCount': recipe['missedIngredientCount'],
-                        'type':'missed__ingredient',
-                        'aisle':item["aisle"],
-                        'usedIngridientCount':recipe['usedIngredientCount'],
-                        'title': recipe['title'],
-                        'name':item['name'],
-                        'unit':item['unit'],
-                        'amount':item["amount"],
-                    }
-                    json.dump(data, file)
-                    file.write('\n')
-                
-                for item in recipe['usedIngredients']:
-                    data = {
-                        'ingredients': ingredients,
-                        'id': recipe['id'],
-                        'missedIngredientCount': recipe['missedIngredientCount'],
-                        'title': recipe['title'],
-                        'aisle':item["aisle"],
-                        'type':'missed__ingredient',
-                        'name':item['name']
-                    }
-                    json.dump(data, file)
-                    file.write('\n')
-    else:
-        print("An error occurred while fetching the recipes.")
-
+    recipes = response.json()
+    with open('C:\\Users\\aakan\\OneDrive\\Desktop\\In\\api\\recipes.json', 'a') as file:
+        for recipe in recipes:
+            for item in recipe["missedIngredients"]:
+                data = {
+                    'ingredients': ingredients,
+                    'id': recipe['id'],
+                    'missedIngredientCount': recipe['missedIngredientCount'],
+                    'type':'missed__ingredient',
+                    'aisle':item["aisle"],
+                    'usedIngridientCount':recipe['usedIngredientCount'],
+                    'title': recipe['title'],
+                    'name':item['name'],
+                    'unit':item['unit'],
+                    'amount':item["amount"],
+                }
+                json.dump(data, file)
+                file.write('\n')
+            
+            for item in recipe['usedIngredients']:
+                data = {
+                    'ingredients': ingredients,
+                    'id': recipe['id'],
+                    'missedIngredientCount': recipe['missedIngredientCount'],
+                    'type':'used__ingredient',
+                    'aisle':item["aisle"],
+                    'usedIngridientCount':recipe['usedIngredientCount'],
+                    'title': recipe['title'],
+                    'name':item['name'],
+                    'unit':item['unit'],
+                    'amount':item["amount"],
+                }
+                json.dump(data, file)
+                file.write('\n')
 ingredients = input("Enter the ingredients of your choice: ")
 getRecipeByIngredients(ingredients)
